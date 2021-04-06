ed32431d707e58c2c93abee47a4559ab686e99ec
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-06 10:54:49 -0500

    exercise completed

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response;
+
+            switch (answer)
+            {
+                case < 9:
+                    response = $"{answer} is less than nine";
+                    break;
+                default:
+                    response = $"{answer} greater than or equal to nine";
+                    break;
+            }
+            Console.WriteLine(response);
+
+        }
+    }
+}
